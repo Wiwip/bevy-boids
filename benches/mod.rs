d14997887b3063bf0c84d4323d38905571c223f6
Wiwip <wiwipaf@gmail.com>
2023-03-25 20:44:04 -0400

    Addes multiple benchmarks for different data structures. Maybe QuadTree left to test.

--- benches/mod.rs
+++ benches/mod.rs
@@ -1,19 +1,11 @@
-use criterion::{criterion_group, criterion_main, Criterion};
 
-mod coherence_bench;
-mod system_bench;
+use bevy::prelude::*;
+use criterion::criterion_main;
 
-criterion_main!(benches);
-criterion_group!(benches, iterator_tests,);
+mod spatial;
 
-pub fn iterator_tests(c: &mut Criterion) {
-    let mut bench = coherence_bench::Benchmark::new();
 
-    // Start benchmark
-    let mut g = c.benchmark_group("Looping Method Comparison");
-    g.bench_function("Hash System Cost", |b| b.iter(|| bench.run_hash_system()));
-    g.bench_function("Iterator", |b| b.iter(|| bench.run_iterator()));
-    g.bench_function("Foreach", |b| b.iter(|| bench.run_forloop()));
-    g.bench_function("Lazy O(n^2)", |b| b.iter(|| bench.run_lazy()));
-    g.finish();
-}
+criterion_main!(
+    spatial::spatial_benches,
+);
+
