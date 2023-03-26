1b30ea5d19671a8898592b05574a7bc9cb0bd43a
Wiwip <wiwipaf@gmail.com>
2023-03-25 20:43:27 -0400

    Added basic controls to the camera in camera_control.rs

---
+++ src/camera_control.rs
@@ -0,0 +1,38 @@
+use bevy::input::Input;
+use bevy::input::mouse::{MouseMotion, MouseScrollUnit, MouseWheel};
+use bevy::prelude::{Camera, EventReader, MouseButton, OrthographicProjection, Query, Res, Transform};
+use bevy_rapier2d::na::clamp;
+
+pub fn camera_drag(
+    mut query: Query<(&Camera, &mut Transform, &OrthographicProjection)>,
+    input: Res<Input<MouseButton>>,
+    mut motion: EventReader<MouseMotion>,
+){
+    let (c, mut tf, proj) = query.single_mut();
+    if input.pressed(MouseButton::Middle){
+        for ev in motion.iter() {
+            tf.translation.x -= ev.delta.x * proj.scale;
+            tf.translation.y += ev.delta.y * proj.scale;
+        }
+    }
+}
+
+pub fn camera_zoom(
+    mut query: Query<(&Camera, &mut OrthographicProjection)>,
+    mut scroll: EventReader<MouseWheel>,
+){
+    let (_, mut proj) = query.single_mut();
+    for ev in scroll.iter() {
+        match ev.unit {
+            MouseScrollUnit::Line => {
+                proj.scale -= ev.y / 10.;
+                println!("Scroll (line units): vertical: {}, horizontal: {}", ev.y, ev.x);
+            }
+            MouseScrollUnit::Pixel => {
+                proj.scale -= ev.y / 10.;
+                println!("Scroll (pixel units): vertical: {}, horizontal: {}", ev.y, ev.x);
+            }
+        }
+        proj.scale = clamp(proj.scale, 0.25, 1.25);
+    }
+}
